05bcf9e9b394a8b72e96f427dff08a4c4baf8f98
RodrigoUchoa001 <franciscorferreira@aluno.uespi.br>
2022-09-14 21:38:46 -0300

    criado rotas da api

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,18 @@
-import express from 'express';
+import express, { json } from 'express';
 
 const app = express();
 
-app.get('/ads', (req, res) => {
+app.get('/games', (req,res) =>{
+    res.json([]);
+})
+
+app.post('/ads', (req,res) =>{
+    res.status(201).json([]);
+})
+
+app.get('/games/:id/ads', (req, res) => {
+    const gameId = req.params.id;
+
     res.json([
         { id: 1, "name": 'abc' },
         { id: 2, "name": 'abc' },
@@ -10,6 +20,12 @@ app.get('/ads', (req, res) => {
         { id: 4, "name": 'abc' },
     ]);
 
+});
+
+app.get('/ads/:id/discord', (req, res) => {
+    const adId = req.params.id;
+
+    res.json([]);
 });
 
 app.listen(3333);
